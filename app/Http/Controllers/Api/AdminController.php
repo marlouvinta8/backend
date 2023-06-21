4bb4e031947ec379cdac5ad98c0e72f6512a40aa
Marlou T. Vinta <marlouvinta8@gmail.com>
2023-06-21 08:30:51 +0800

    removing some code

--- app/Http/Controllers/Api/AdminController.php
+++ app/Http/Controllers/Api/AdminController.php
@@ -27,38 +27,38 @@ class AdminController extends Controller
     }
 
     //para masave ang ginawang admin account
-    public function store(Request $request){
-        $validator = Validator::make($request->all(), [
-            'username' => 'required|string|max:191',
-            'password' => 'required|string|min:5',
-        ]);
+    // public function store(Request $request){
+    //     $validator = Validator::make($request->all(), [
+    //         'username' => 'required|string|max:191',
+    //         'password' => 'required|string|min:5',
+    //     ]);
 
-        if($validator->fails()){
+    //     if($validator->fails()){
 
-            return response()->json([
-                'status' => 402,
-                'admin' => $validator->messages()
-            ],402);
-        }else{
-            $admin = Admin::create([
-                'username' => $request->username,
-                'password' => $request->password,
-            ]);
+    //         return response()->json([
+    //             'status' => 402,
+    //             'admin' => $validator->messages()
+    //         ],402);
+    //     }else{
+    //         $admin = Admin::create([
+    //             'username' => $request->username,
+    //             'password' => $request->password,
+    //         ]);
 
-            if($admin){
+    //         if($admin){
 
-                return response()->json([
-                    'status' => 200,
-                    'message' => "Admin Created Successfully"
-                ],200);
-            }else{
-                return response()->json([
-                    'status' => 500,
-                    'message' => "Something Went Wrong!"
-                ],500);
-            }
-        }
-     }
+    //             return response()->json([
+    //                 'status' => 200,
+    //                 'message' => "Admin Created Successfully"
+    //             ],200);
+    //         }else{
+    //             return response()->json([
+    //                 'status' => 500,
+    //                 'message' => "Something Went Wrong!"
+    //             ],500);
+    //         }
+    //     }
+    //  }
 
      //kapag mageedit ng account
      public function edit($id){
