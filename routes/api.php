48dd8749025451e107868ac322744c577fed18af
Marlou T. Vinta <marlouvinta8@gmail.com>
2023-06-23 13:48:47 +0800

    testing

--- routes/api.php
+++ routes/api.php
@@ -22,7 +22,7 @@ Route::delete('member/{id}/delete', [MemberController::class, 'destroy']);
 
 // for admin account
 Route::get('login', [AdminController::class, 'index']);
-Route::post('login', [AdminController::class, 'store'])->name('store');
+Route::post('login', [AdminController::class, 'store']);
 Route::get('login/{id}/edit', [AdminController::class, 'edit']);
 Route::put('login/{id}/edit', [AdminController::class, 'update']);
 Route::post('/logout', [AdminController::class, 'logout'])->name('logout');
